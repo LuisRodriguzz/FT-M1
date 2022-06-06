a6e7f6f837b8afc5023a084825812a5b4260f4b4
unknown <luisrodriguzzr@gmail.com>
2022-06-06 15:46:20 -0500

    Actividad 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,11 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+let resultado = 0
+let numero = num.split("")
+let Potenciador = numero.length - 1
+
+for (let i=0; i < numero.length; i++) {
+  resultado += numero[i] * Math.pow(2,Potenciador);
+  Potenciador --
+}
+return resultado;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let numero = num
+  let resultado = ""
+  while (numero > 0) {
+    resultado = numero % 2 + resultado
+    numero = Math.floor(numero/2);
+  }
+  return resultado;
 
 }
 
